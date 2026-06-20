1f7f82b606e9fa058046aebebe330ee543945040
Samuel Pasquali <samuelsamuelpasquali13@gmail.com>
2026-06-19 23:47:38 -0300

    a

--- src/Exercicios/M4/stb_image.cpp
+++
@@ -1,3 +0,0 @@
-// STB Image Implementation - Single file with progressive JPEG support
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
--- src/Exercicios/M5/Layer.h
+++
@@ -1,6 +0,0 @@
-typedef struct Layer {
-    float z;
-    unsigned int tid;
-    const char* filename;
-    float offsetx, offsety, ratex, ratey;
-} Layer;
